a4d2a7b585efcdd0445a7ee43885a518b77c0be0
kone Adama <demsei19@gmail.com>
2026-09-25 13:42:20 +0000

    design: refonte esthétique VIP Exclusif (skill frontend-design)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,10 +6,10 @@
     <title>@yield('title', 'Hôtel Kilimandjaro | Évasion & Luxe')</title>
     <meta name="description" content="@yield('meta_description', 'Découvrez l\'Hôtel Kilimandjaro, votre prochaine destination de rêve alliant confort, luxe et nature.')">
     
-    <!-- Google Fonts: Outfit -->
+    <!-- Google Fonts: Outfit & Playfair Display -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
