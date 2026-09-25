499837d5cb475276df34c3084d342c5af8a2720c
kone Adama <demsei19@gmail.com>
2026-09-25 11:58:02 +0000

    feat: creation des modeles RoomType et Room

---
+++ app/Models/Room.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    protected $fillable = ['room_type_id', 'number', 'is_active'];
+
+    public function roomType()
+    {
+        return $this->belongsTo(RoomType::class);
+    }
+}
