e5883a37d288222cba9da5ada7ef3ac7f4e00bd7
kone Adama <demsei19@gmail.com>
2026-09-25 12:54:52 +0000

    fix: ajout relation reservations sur le modele Room

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -12,4 +12,9 @@ class Room extends Model
     {
         return $this->belongsTo(RoomType::class);
     }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
